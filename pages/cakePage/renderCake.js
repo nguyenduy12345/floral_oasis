384f4c69988145df296910688b972be3ab7fc9df
nguyen duy <nguyenvanduy2121999@gmail.com>
2024-04-20 18:39:38 +0700

    update 6:39

--- pages/cakePage/renderCake.js
+++ pages/cakePage/renderCake.js
@@ -120,7 +120,7 @@ function renderOtherProduct(){
     let listPosts = document.querySelectorAll('.product_list_item') 
         listPosts.forEach(listPost =>{
             listPost.onclick = () =>{
-                window.location.href = `../pages/showThatProduct/showproduct.html?productid=${listPost.id}`
+                window.location.href = `../showThatProduct/showproduct.html?productid=${listPost.id}`
         }
     })
 }
